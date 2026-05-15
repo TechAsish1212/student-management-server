1610e7e0658b617ef53804f68620b483c32b0c3a
TechAsish1212 <gitasish1212@gmail.com>
2026-05-15 23:43:44 +0530

    feat: generate jwt token

---
+++ src/utils/generateToken.ts
@@ -0,0 +1,18 @@
+import { Response } from "express";
+import jwt from "jsonwebtoken";
+
+export const generateToken=(userId:string,res:Response)=>{
+    const token=jwt.sign({userId},process.env.JWT_SECRET as string,{
+        expiresIn:'30d',
+        algorithm:'HS512'
+    });
+
+    // attach token to http-only   
+    res.cookie('jwt',token,{
+        httpOnly:true,
+        secure:process.env.NODE_ENV==='production',
+        sameSite:"strict",
+        maxAge:30*24*60*60*1000,
+        path:'/'
+    })
+}
