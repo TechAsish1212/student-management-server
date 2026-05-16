0a769b3f2b8b5d4af756690cb03345b4af57f637
TechAsish1212 <gitasish1212@gmail.com>
2026-05-17 00:22:48 +0530

    feat: add get all users,get user profile and logout routes

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { deleteUser, signin, signup, updateUser } from "../controllers/user.controller";
+import { deleteUser, getAllUsers, getUserProfile, logout, signin, signup, updateUser } from "../controllers/user.controller";
 import { authorize, authProtect } from "../middlewares/auth";
 
 const authRouter=Router();
@@ -8,6 +8,9 @@ authRouter.post('/signup',authProtect,authorize(["admin","teacher"]),signup);
 authRouter.post('/signin',signin);
 authRouter.patch('/update/:id',authProtect,authorize(["admin","teacher"]),updateUser);
 authRouter.delete('/delete/:id',authProtect,authorize(["admin","teacher"]),deleteUser);
+authRouter.get('/profile',authProtect,getUserProfile);
+authRouter.post('/logout',logout);
+authRouter.get('/',authProtect,authorize(['admin','teacher']),getAllUsers);
 
 
 
