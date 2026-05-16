763d4de4bf73e874f7cbfe0e5b7adceea73ffa72
TechAsish1212 <gitasish1212@gmail.com>
2026-05-16 21:01:55 +0530

    feat: add update user routes

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { signin, signup } from "../controllers/user.controller";
+import { signin, signup, updateUser } from "../controllers/user.controller";
 import { authorize, authProtect } from "../middlewares/auth";
 
 const authRouter=Router();
 
 authRouter.post('/signup',authProtect,authorize(["admin","teacher"]),signup);
 authRouter.post('/signin',signin);
+authRouter.patch('/update/:id',authProtect,authorize(["admin","teacher"]),updateUser);
 
 
 export default authRouter;
