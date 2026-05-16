98d5aaf4e3aadeeed6167f551608c492eed5afa4
TechAsish1212 <gitasish1212@gmail.com>
2026-05-16 23:01:07 +0530

    feat: add delete route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { signin, signup, updateUser } from "../controllers/user.controller";
+import { deleteUser, signin, signup, updateUser } from "../controllers/user.controller";
 import { authorize, authProtect } from "../middlewares/auth";
 
 const authRouter=Router();
@@ -7,6 +7,8 @@ const authRouter=Router();
 authRouter.post('/signup',authProtect,authorize(["admin","teacher"]),signup);
 authRouter.post('/signin',signin);
 authRouter.patch('/update/:id',authProtect,authorize(["admin","teacher"]),updateUser);
+authRouter.delete('/delete/:id',authProtect,authorize(["admin","teacher"]),deleteUser);
+
 
 
 export default authRouter;
