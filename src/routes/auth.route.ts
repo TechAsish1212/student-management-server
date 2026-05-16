77fa2e3200d2289cfab459f61f1fe928534878fe
TechAsish1212 <gitasish1212@gmail.com>
2026-05-16 11:43:16 +0530

    feat: add protect route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { signin, signup } from "../controllers/user.controller";
+import { authorize, authProtect } from "../middlewares/auth";
 
 const authRouter=Router();
 
-authRouter.post('/signup',signup);
+authRouter.post('/signup',authProtect,authorize(["admin","teacher"]),signup);
 authRouter.post('/signin',signin);
 
 
