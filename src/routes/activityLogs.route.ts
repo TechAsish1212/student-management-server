28bccb9821f47e7522cc3e1d33861839357786c1
TechAsish1212 <gitasish1212@gmail.com>
2026-05-16 19:54:11 +0530

    feat: activity log with index

--- src/routes/activityLogs.route.ts
+++ src/routes/activityLogs.route.ts
@@ -1,4 +1,9 @@
 import { Router } from "express";
+import { getActivities } from "../controllers/activity.controller";
+import { authorize, authProtect } from "../middlewares/auth";
 
-const activityRouter=Router();
+const activityRouter = Router();
 
+activityRouter.get('/', authProtect, authorize(["admin", "teacher"]), getActivities);
+
+export default activityRouter;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRouter from "./auth.route";
+import activityRouter from "./activityLogs.route";
 
 const routes=Router();
 
 routes.use('/auth',authRouter);
+routes.use('/activities',activityRouter);
 
 export default routes;
