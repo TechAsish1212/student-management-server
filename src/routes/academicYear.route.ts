1e92f74051ec5c094b7d367374c3641fb8706fcf
TechAsish1212 <gitasish1212@gmail.com>
2026-05-17 23:24:20 +0530

    feat: add update academic year route

--- src/routes/academicYear.route.ts
+++ src/routes/academicYear.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createAcademicYear, getCurrentAcademicYear } from "../controllers/academicYear.controller";
+import { createAcademicYear, getCurrentAcademicYear, updateAcademicYear } from "../controllers/academicYear.controller";
 import { authorize, authProtect } from "../middlewares/auth";
 
 const academicYearRoute=Router();
 academicYearRoute.post('/create',authProtect,authorize(['admin']),createAcademicYear);
 academicYearRoute.get('/current',authProtect,getCurrentAcademicYear);
+academicYearRoute.patch('/update/:id',authProtect,updateAcademicYear)
 
 export default academicYearRoute;
