8fb57a34f1573ae59d8169ffe880a7f8618804ad
TechAsish1212 <gitasish1212@gmail.com>
2026-05-17 23:23:36 +0530

    feat: create function for update academic year

--- src/controllers/academicYear.controller.ts
+++ src/controllers/academicYear.controller.ts
@@ -55,3 +55,42 @@ export const getCurrentAcademicYear = async (req: Request, res: Response) => {
         throw new ApiError(500, "Internal server Error")
     }
 }
+
+// update academic year
+export const updateAcademicYear = async (req: Request, res: Response) => {
+    try {
+        const { name, fromYear, toYear, isCurrent } = req.body;
+        const academicYear = await AcademicYear.findById(req.params.id);
+        if (!academicYear) {
+            throw new ApiError(404, "Academic year not found");
+        }
+        if (isCurrent) {
+            await AcademicYear.updateMany({}, { $set: { isCurrent: false } });
+        }
+
+        academicYear.name = name || academicYear.name;
+        academicYear.fromYear = fromYear || academicYear.fromYear;
+        academicYear.toYear = toYear || academicYear.toYear;
+        academicYear.isCurrent !== undefined ? isCurrent : academicYear.isCurrent;
+
+        await academicYear.save();
+
+        await activityLog({
+            userId: (req as any).user._id,
+            action: `Updated Academic Year ${academicYear.name}`,
+        });
+
+        return res.status(200).json({
+            message: 'Academic year updated successfully',
+            academicYear
+        })
+
+
+    } catch (error) {
+        console.log("Update academic year error:: ", error);
+        if (error instanceof ApiError) {
+            throw error;
+        }
+        throw new ApiError(500, "Internal Server Error");
+    }
+}
