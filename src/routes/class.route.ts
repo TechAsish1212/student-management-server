041c99bd35787a3d2827617063a9a48fb8f21780
TechAsish1212 <gitasish1212@gmail.com>
2026-05-20 22:12:37 +0530

    feat: add delete routes

--- src/routes/class.route.ts
+++ src/routes/class.route.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { authorize, authProtect } from "../middlewares/auth";
-import { createClass, updateClass } from "../controllers/class.controller";
+import { createClass, deleteClass, getAllClass, updateClass } from "../controllers/class.controller";
 
 const classRouter=Router();
 classRouter.post('/create',authProtect,authorize(['admin']),createClass);
 classRouter.patch('/update/:id',authProtect,authorize(['admin']),updateClass);
-
+classRouter.delete('/delete/:id',authProtect,authorize(['admin']),deleteClass);
 
 export default classRouter;
