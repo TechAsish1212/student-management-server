13a4abbf9c86b89865ad4ecffaea0855ca8ff9cb
TechAsish1212 <gitasish1212@gmail.com>
2026-05-22 00:50:57 +0530

    feat: add delete subjects routes

--- src/routes/subject.route.ts
+++ src/routes/subject.route.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { authorize, authProtect } from "../middlewares/auth";
-import { createSubject, getAllSubject, updateSubject } from "../controllers/subject.controller";
+import { createSubject, deleteSubject, getAllSubject, updateSubject } from "../controllers/subject.controller";
 
 const subjectRoute=Router();
 
 subjectRoute.post('/create',authProtect,authorize(['admin','teacher']),createSubject);
 subjectRoute.patch('/update/:id',authProtect,authorize(['admin','teacher']),updateSubject);
 subjectRoute.get('/',authProtect,authorize(['admin','teacher']),getAllSubject);
+subjectRoute.delete('/delete/:id',authProtect,authorize(['admin','teacher']),deleteSubject);
 
 
 
