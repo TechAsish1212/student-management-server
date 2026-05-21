761009ab8d81a262873f4d91f2d5f3dc761a0de3
TechAsish1212 <gitasish1212@gmail.com>
2026-05-22 00:37:16 +0530

    feat: add get all subjects routes

--- src/routes/subject.route.ts
+++ src/routes/subject.route.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import { authorize, authProtect } from "../middlewares/auth";
-import { createSubject, updateSubject } from "../controllers/subject.controller";
+import { createSubject, getAllSubject, updateSubject } from "../controllers/subject.controller";
 
 const subjectRoute=Router();
 
 subjectRoute.post('/create',authProtect,authorize(['admin','teacher']),createSubject);
 subjectRoute.patch('/update/:id',authProtect,authorize(['admin','teacher']),updateSubject);
+subjectRoute.get('/',authProtect,authorize(['admin','teacher']),getAllSubject);
+
+
 
 export default subjectRoute;
