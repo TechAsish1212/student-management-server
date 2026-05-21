9fb86a0f398f7f65bfbd2ec9326d2abc8ce6d069
TechAsish1212 <gitasish1212@gmail.com>
2026-05-21 22:06:48 +0530

    feat: add routes for update

--- src/routes/subject.route.ts
+++ src/routes/subject.route.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { authorize, authProtect } from "../middlewares/auth";
-import { createSubject } from "../controllers/subject.controller";
+import { createSubject, updateSubject } from "../controllers/subject.controller";
 
 const subjectRoute=Router();
 
 subjectRoute.post('/create',authProtect,authorize(['admin','teacher']),createSubject);
+subjectRoute.patch('/update/:id',authProtect,authorize(['admin','teacher']),updateSubject);
 
 export default subjectRoute;
