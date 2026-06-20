9b510cae07d99f73cd8f64497f4f63a50a4a4b11
TechAsish1212 <gitasish1212@gmail.com>
2026-06-21 02:05:56 +0530

    feat: add routes for forgot password and reset password

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,16 +1,20 @@
 import { Router } from "express";
-import { deleteUser, getAllUsers, getUserProfile, logout, signin, signup, updateUser } from "../controllers/user.controller";
+import { deleteUser, forgotPass, getAllUsers, getUserProfile, logout, resetPassword, signin, signup, updateUser } from "../controllers/user.controller";
 import { authorize, authProtect } from "../middlewares/auth";
 
 const authRouter=Router();
 
-authRouter.post('/signup',authProtect,authorize(["admin","teacher"]),signup);
+authRouter.post('/signup',signup);
 authRouter.post('/signin',signin);
 authRouter.patch('/update/:id',authProtect,authorize(["admin","teacher"]),updateUser);
 authRouter.delete('/delete/:id',authProtect,authorize(["admin","teacher"]),deleteUser);
 authRouter.get('/profile',authProtect,getUserProfile);
 authRouter.post('/logout',logout);
 authRouter.get('/',authProtect,authorize(['admin','teacher']),getAllUsers);
+
+// forgot pass
+authRouter.post('/forgot-password',forgotPass);
+authRouter.post("/reset-password/:token", resetPassword);
 
 
 
